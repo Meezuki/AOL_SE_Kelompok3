5db64466c943b5655db17dd5dff004fc9defb1cb
Marco <marcoodenleo25@gmail.com>
2026-05-13 13:57:12 +0700

    homepage event page

--- script_home.js
+++ script_home.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // Daftar URL gambar untuk background (Ganti dengan path gambarmu nanti)
     const heroImages = [
         'assets/hero1.jpg', // Gambar 1 (Orang bagi-bagi makanan)
-        'https://images.unsplash.com/photo-1559027615-cd4628902d4a?q=80&w=1920&auto=format&fit=crop', // Gambar 2 (Orang menanam pohon)
-        'https://images.unsplash.com/photo-1529390079861-591de354faf5?q=80&w=1920&auto=format&fit=crop'  // Gambar 3 (Mengajar anak-anak)
+        'assets/hero2.jpg', // Gambar 2 (Orang menanam pohon)
+        'assets/hero3.jpg'  // Gambar 3 (Mengajar anak-anak)
     ];
 
     let currentIndex = 0;
@@ -20,5 +20,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Jalankan pergantian gambar setiap 5 detik (5000 milidetik)
-    setInterval(cycleHeroImage, 5000);
+    setInterval(cycleHeroImage, 3000);
 });
